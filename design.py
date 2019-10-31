be8cc5d41dae0ced1f693f9ee8a2114b64b9959d
Em <emalee.keesler@gmail.com>
2019-10-31 11:44:18 -0500

    Update design.py

--- design.py
+++ design.py
@@ -2,15 +2,42 @@
 from tkinter import *
 tk = Tk()
 
+class Checkbar(Frame):
+   def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
+      Frame.__init__(self, parent)
+      self.vars = []
+      for pick in picks:
+         var = IntVar()
+         chk = Checkbutton(self, text=pick, variable=var)
+         chk.pack(side=side, anchor=anchor, expand=YES)
+         self.vars.append(var)
+   def state(self):
+      return map((lambda var: var.get()), self.vars)
+
 def orders():
         tk2 = Tk()
         tk.withdraw()
         tk2.title("Order")
         Label1 = Label(tk2, text = "What menu would you like to order from?", font =('Caviar Dreams',40), bg = 'white', fg = 'black')
         Label1.pack(pady=10,padx=10)
-        food = Button(tk2, text='Food',font =('Caviar Dreams' ,26), width=20, height=3, fg='green').pack()
+        food = Button(tk2, text='Food',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[foods.__call__(), tk2.withdraw()]).pack()
         drink = Button(tk2, text='Drink',font =('Caviar Dreams' ,26), width=20, height=3, fg='green').pack()
         back = Button(tk2, text='Back',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tk2.withdraw(),tk.deiconify()]).pack()
+
+def foods():
+        tk3 = Tk()
+        tk3.title("Food")
+        Label1 = Label(tk3, text = "Select an option", font =('Caviar Dreams',40), bg = 'white', fg = 'black')
+        pizza = Button(tk3, text = "Pizza", font = ('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[pizzas.__call__(), tk3.withdraw()]).pack()
+        pasta = Button(tk3, text = "Pasta", font = ('Caviar Dreams' ,26), width=20, height=3, fg='green').pack()
+        salad = Button(tk3, text = "Salad", font = ('Caviar Dreams' ,26), width=20, height=3, fg='green').pack()
+        back2 = Button(tk3, text='Back',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tk3.withdraw(),tk2.deiconify()]).pack()
+
+def pizzas():
+        tkP = Tk()
+        opt = Checkbar(tkP, ['Pepperoni', 'Bacon', 'Sausage', 'Spinach', 'Mushroom', 'Olives'])
+        opt.pack(side=TOP,  fill=X)
+        back2 = Button(tkP, text='Back',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tkP.withdraw(),tk3.deiconify()]).pack()
 
 def main(): 
         #tk = Tk() 
