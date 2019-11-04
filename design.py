0d1d6c3d6c8d74ff955fb79edb8a07f3cc8d960a
RubenOr <49449377+RubenOr@users.noreply.github.com>
2019-11-04 17:11:44 -0600

    Update design.py
    working on edit menu

--- design.py
+++ design.py
@@ -1,70 +1,94 @@
 
 from tkinter import *
 import webbrowser
+
 tk = Tk()
-tk.wm_attributes("-fullscreen", True)
+tk.wm_attributes("-fullscreen", False)
 
 ticket = []
 
-def requests():
-        tkr = Tk()
-        tkr.wm_attributes("-fullscreen", True)
-        Labelr = Label(tkr, text = "Your server has been notified and will be by shortly.", font =('Caviar Dreams',40), bg = 'white', fg = 'black')
-        Labelr.pack(pady=400,padx=10)
-        backr = Button(tk3, text='Back',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tkr.withdraw(),tk.deiconify()]).pack()
 
-def orders():
+def editMenus():
         tk2 = Tk()
-        tk2.wm_attributes("-fullscreen", True)
+        tk2.wm_attributes("-fullscreen", False)
         tk.withdraw()
-        tk2.title("Order")
-        Label1 = Label(tk2, text = "What menu would you like to order from?", font =('Caviar Dreams',40), bg = 'white', fg = 'black')
+        tk2.title("Edit Menu")
+        Label1 = Label(tk2, text = "Choose menu to edit: ", font =('Arial',40), bg = 'white', fg = 'black')
         Label1.pack(pady=200,padx=10)
-        food = Button(tk2, text='Food',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[foods.__call__(), tk2.withdraw()]).pack()
-        drink = Button(tk2, text='Drink',font =('Caviar Dreams' ,26), width=20, height=3, fg='green').pack()
-        back = Button(tk2, text='Back',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tk2.withdraw(),tk.deiconify()]).pack()
+        Button(tk2, text='Food Menu',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[foods.__call__(), tk2.withdraw()]).pack()
+        Button(tk2, text='Drink Menu',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[drinks.__call__(),tk2.withdraw()]).pack()
+        Button(tk2, text='Cancel',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[tk2.withdraw(),tk.deiconify()]).pack()
 
 def foods():
         tk3 = Tk()
-        tk3.wm_attributes("-fullscreen", True)
+        tk3.wm_attributes("-fullscreen", False)
         tk3.title("Food")
-        Label1 = Label(tk3, text = "Select an option", font =('Caviar Dreams',40), bg = 'white', fg = 'black')
-        pizza = Button(tk3, text = "Pizza", font = ('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[pizzas(), tk3.withdraw()]).pack()
-        pasta = Button(tk3, text = "Pasta", font = ('Caviar Dreams' ,26), width=20, height=3, fg='green').pack()
-        salad = Button(tk3, text = "Salad", font = ('Caviar Dreams' ,26), width=20, height=3, fg='green').pack()
-        back2 = Button(tk3, text='Back',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tk3.withdraw(),tk.deiconify()]).pack()
+        Label(tk3, text = "Food Options:", font =('Arial',40), bg = 'white', fg = 'black').pack(pady=100)
+        # pizzas
+        Label(tk3, text = "Pizza", font = ('Arial' ,26), bg='white', fg='black').pack()
+        pizzas = []
+        p = ['Water', 'Milk', 'Sausage', 'Spinach', 'Mushroom', 'Olives', 'Pineapple']
+        
+        for pick in p:
+                x = IntVar()
+                Checkbutton(tk3, text=pick, variable=x, font=('Arial', 26)).pack()
+                pizzas.append(x)
+
+        # pastas
+        Label(tk3, text = "Pastas", font = ('Arial' ,26), bg='white', fg='black').pack()
+
+        # Salads
+        Label(tk3, text = "Salads", font = ('Arial' ,26), bg='white', fg='black').pack()
+        back2 = Button(tk3, text='Back',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[tk3.destroy(),tk.deiconify()]).pack()
+        tk3.mainloop()
 
 def pizzas():
         tkP = Tk()
-        tkP.wm_attributes("-fullscreen", True)
+        tkP.wm_attributes("-fullscreen", False)
         vars = []
-        Labelp = Label(tkP, text = "What would you like on your pizza?", font =('Caviar Dreams',40), bg = 'white', fg = 'black')
+        tk2.withdraw()
+        Labelp = Label(tkP, text = "What would you like on your pizza?", font =('Arial',40), bg = 'white', fg = 'black')
         Labelp.pack(pady=100,padx=100)
         picks = ['Pepperoni', 'Bacon', 'Sausage', 'Spinach', 'Mushroom', 'Olives', 'Pineapple']
         for pick in picks:
                 var = IntVar()
-                chk = Checkbutton(tkP, text=pick, variable=var, font =('Caviar Dreams' ,26)).pack()
+                chk = Checkbutton(tkP, text=pick, variable=var, font =('Arial' ,26)).pack()
                 vars.append(var)
 
-        add = Button(tkP, text='Add to Order',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tkP.withdraw(), ticket.append("Pizza")]).pack()
-        back4 = Button(tkP, text='Back',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command=lambda:[tkP.withdraw(),tk.deiconify()]).pack()
+        add = Button(tkP, text='Add to Order',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[tkP.withdraw(), ticket.append("Drinks")]).pack()
+        back4 = Button(tkP, text='Back',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[tkP.withdraw(),tk.deiconify()]).pack()
+
+def drinks():
+        tkP = Tk()
+        tkP.wm_attributes("-fullscreen", False)
+        vars = []
+        Labelp = Label(tkP, text = "What drinks would you like?", font =('Arial',40), bg = 'white', fg = 'black')
+        Labelp.pack(pady=100,padx=100)
+        picks = ['Dr. Pepper', 'Pepsi', 'Sausage', 'Mountain Dew', 'Strawberry Lemonade', 'Lemonade', 'Water']
+        for pick in picks:
+                var = IntVar()
+                chk = Checkbutton(tkP, text=pick, variable=var, font =('Arial' ,26)).pack()
+                vars.append(var)
+
+        add = Button(tkP, text='Add to Order',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[tkP.withdraw(), ticket.append("Pizza")]).pack()
+        back4 = Button(tkP, text='Back',font =('Arial' ,26), width=20, height=3, fg='green', command=lambda:[tkP.withdraw(),tk.deiconify()]).pack()
 
 def game():
         webbrowser.open_new("https://www.solitr.com")
 
 def paycheck():
         tkpc = Tk()
-        tkpc.wm_attributes("-fullscreen", True)
-        Labelc0 = Label(tkpc, text = "Your check:" ,font =('Caviar Dreams',40), bg = 'white', fg = 'green').pack()
+        tkpc.wm_attributes("-fullscreen", False)
+        Label(tkpc, text = "Your check:" ,font =('Arial',40), bg = 'white', fg = 'green').pack()
         Labelc = Label(tkpc, text = 
-        "  Pizza                $8.00\n    Fetticine            $11.00\n  Cabernet(2)       $8.50\n\n\n  Tax:                  $2.27\n   Total:               $29.77", font =('Caviar Dreams',40), bg = 'white', fg = 'black')
+        "  Pizza                $8.00\n    Fetticine            $11.00\n  Cabernet(2)       $8.50\n\n\n  Tax:                  $2.27\n   Total:               $29.77", font =('Arial',40), bg = 'white', fg = 'black')
         Labelc.pack(pady=100,padx=100,)
-        Labelc1 = Label(tkpc, text = "Your server has been notified and a paper copy is on it's way." ,font =('Caviar Dreams',40), bg = 'white', fg = 'green').pack()
-        ex = Button(tkpc, text="Exit", font=('Caviar Dreams',26), command=lambda:[tkpc.withdraw(), tk.deiconify()]).pack()
+        Label(tkpc, text = "Your server has been notified and a paper copy is on it's way." ,font =('Arial',40), bg = 'white', fg = 'green').pack()
+        Button(tkpc, text="Exit", font=('Arial',26), command=lambda:[tkpc.withdraw(), tk.deiconify()]).pack()
 
 def main(): 
         #tk = Tk() 
-        tk.title('Customer Streamliner 9000') 
+        tk.title('Customer Streamliner 9000 [ADMIN]') 
         # Taskbar / Menu 
         menu = Menu(tk)
         tk.config(menu=menu)
@@ -73,24 +97,24 @@ def main():
         file.add_command(label="Exit", command=tk.destroy)
 
         #Logo added
-        frame = Frame(tk, width=800, height=600, background='white')
-        frame.pack_propagate(0)    
-        frame.pack()
-        # Logo that we created
-        img = PhotoImage(file='ivancicitalianv2.png')
-        pic = Label(frame, image=img)
-        pic.pack()
+        # frame = Frame(tk, width=800, height=600, background='white')
+        # frame.pack_propagate(0)    
+        # frame.pack()
+        # # Logo that we created
+        # img = PhotoImage(file='ivancicitalianv2.png')
+        # pic = Label(frame, image=img)
+        # pic.pack()
 
         #Buttons
-        service = Button(tk, text='Request Service', width=20, height=3, font =('Caviar Dreams',26), fg='green', command = requests)
+        service = Button(tk, text='Edit Menu', width=20, height=3, font=('Arial',26), fg='green', command = editMenus)
         service.pack(side=LEFT)
-        order = Button(tk, text='Order',font =('Caviar Dreams' ,26), width=20, height=3, fg='green', command = orders) 
+        order = Button(tk, text='View Tables' , width=20, height=3,font=('Arial' ,26),  fg='green', command = editMenus) 
         order.pack(side = LEFT)
-        check = Button(tk, text='Request Check', width=20, height=3, font =('Caviar Dreams',26), fg='green', command = paycheck)
+        check = Button(tk, text='Request Check', width=20, height=3, font=('Arial',26), fg='green', command = paycheck)
         check.pack(side=RIGHT)
-        pay = Button(tk, text='Make Payment', width=20,height=3, font =('Caviar Dreams',26), fg='green') 
+        pay = Button(tk, text='Make Payment', width=20,height=3, font=('Arial',26), fg='green') 
         pay.pack(side=RIGHT)
-        games = Button(tk, text='Games', width=20, height=3, font =('Caviar Dreams' ,26), fg='green', command = game)
+        games = Button(tk, text='Games', width=20, height=3, font=('Arial' ,26), fg='green', command = game)
         games.pack(side=LEFT)
 
         tk.mainloop()
